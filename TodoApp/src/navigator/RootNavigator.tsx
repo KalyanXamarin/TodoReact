f76a0170e7b087db7fb6a5f4f431842bdfb0abad
Kalyan Pidugu <>
2023-02-06 07:38:08 +0530

    Working on login screen

---
+++ TodoApp/src/navigator/RootNavigator.tsx
@@ -0,0 +1,39 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import LoginView from "../views/LoginView"
+import HomeView from "../views/HomeView"
+import RegisterView from "../views/RegisterView"
+
+const Stack = createNativeStackNavigator()
+
+const RootNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#047ABC'
+                    },
+                    headerTintColor: '#ffffff',
+                    headerTitleStyle: {
+                        fontSize: 25,
+                        fontWeight: 'bold'
+                    }
+                }}>
+                <Stack.Screen
+                    name='Login'
+                    component={LoginView}/>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeView}/>
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterView}/>
+           </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default RootNavigator
---
+++ TodoApp/src/views/LoginView.tsx
@@ -0,0 +1,58 @@
+import { View, Text, Image, StyleSheet } from "react-native"
+
+const LoginView = () => {
+    return(
+        <View style={styles.rootElementStyle}>
+            <Image style={styles.imageStyle} source={require("../../assets/icons/appicon.png")}/>
+        </View>
+    )
+} 
+
+const styles = StyleSheet.create({
+    rootElementStyle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    imageStyle: {
+        marginBottom: 40,
+        height: 100,
+        width: 100
+    },
+
+    inputView: {
+        backgroundColor: '#E4E8F0',
+        borderRadius: 5,
+        width: '80%',
+        padding: 10,
+        marginBottom: 20,
+        alignItems: 'center'
+    },
+
+    loginButton: {
+        width: '80%',
+        borderRadius: 5, 
+        backgroundColor: '#2E5BFF',
+        padding: 10,
+        marginBottom: 20,
+        alignItems: 'center'
+    },
+
+    loginText: {
+        color: '#FFFFFF'
+    },
+
+    registerTextStyle: {
+        color: '#000000',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14,
+        alignSelf: 'center',
+        padding: 10,
+        marginTop: 15
+    }
+
+})
+
+export default LoginView
